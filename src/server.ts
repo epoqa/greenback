f48cb26f120a3445430a751044c3d99db2db6db2
LashaMarkhvaidze <lasha.marxvaidze@gmail.com>
2021-12-22 15:55:41 +0400

    eslint initialized

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
-import express, {Application, Request, Response, NextFunction} from "express";
+import express, { Application, Request, Response, NextFunction } from 'express';
 
 const app: Application = express();
 const PORT = process.env.PORT || 3333;
 
-app.get('/', (req:Request, res:Response, next:NextFunction ) => { 
-    res.send('Hello World!');
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+  res.send('Hello World!');
 });
+console.log('asf');
 
-app.listen(PORT, () => {console.log(`http://localhost:${PORT}`)})
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
+});
