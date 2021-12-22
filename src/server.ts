79fe2fae4c2983f50d43c57178470ac40cf81f9b
LashaMarkhvaidze <lasha.marxvaidze@gmail.com>
2021-12-22 12:11:20 +0400

    structure

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express, {Application, Request, Response, NextFunction} from "express";
+
+const app: Application = express();
+const PORT = process.env.PORT || 3333;
+
+app.get('/', (req:Request, res:Response, next:NextFunction ) => { 
+    res.send('Hello World!');
+});
+
+app.listen(PORT, () => {console.log(`http://localhost:${PORT}`)})
