1048dee9021c8ef35ccdc74c8396ab9575759f14
LashaMarkhvaidze <lasha.marxvaidze@gmail.com>
2022-01-10 19:00:02 +0400

    m

--- src/models/diary.js
+++ src/models/diary.js
@@ -1,49 +1,48 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const Diary = mongoose.model('Diary', {
-    diaryName: {
-        type: String,
-        required: true,
-        trim: true
+  diaryName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  type: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: false,
+    trim: true,
+  },
+  owner: {
+    type: String,
+    required: true,
+    ref: 'User',
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  pictures: [
+    {
+      createdAt: {
+        type: Date,
+        default: Date.now,
+      },
+      picture: String,
     },
-    type: {
-        type: String,
-        required: true,
-        trim: true
+  ],
+  comments: [
+    {
+      owner: String,
+      comment: String,
+      createdAt: {
+        type: Date,
+        default: Date.now,
+      },
     },
-    description: {
-        type: String,
-        required: false,
-        trim: true
-    },
-    owner: {
-        type: String,
-        required: true,
-        ref: 'User'
-    },
-    createdAt: {
-        type: Date, 
-        default: Date.now
-    },
-    pictures: [
-        {
-            createdAt: {
-                type: Date, 
-                default: Date.now
-            },
-            picture: String
-        }
-    ],
-    comments:  [
-        {
-            owner: String,
-            comment: String,         
-            createdAt: {
-                type: Date,
-                default: Date.now
-            }
-            
-        }
-    ]
-})   
-module.exports = Diary
+  ],
+});
+module.exports = Diary;
