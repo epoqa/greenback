1bc7937d1a635032cdeb60b2f8ab1a472ff59019
lukaakhalbedashvili <lukaakhalbedashvili@gmail.com>
2022-02-15 21:43:44 +0400

    test cors fix

--- src/server.js
+++ src/server.js
@@ -1,23 +1,20 @@
 const express = require('express')
 
 const app = express()
+const cors = require('cors')
+
+app.use(cors())
+
 const PORT = process.env.PORT || 3332
 
 const userRouter = require('./routers/user')
 const bodyParser = require('body-parser')
-const cors = require('cors')
 const diaryRouter = require('./routers/diary')
 const mergeRouter = require('./routers/merge')
 require('./db/connection')
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
-
-app.use(
-	cors({
-		credentials: true,
-	})
-)
 
 app.use(express.json())
 app.use(userRouter)
