77f3e8605e364361ccadd96e1e0d769034082e38
lashamarkhvaidze <lasha.marxvaidze@gmail.com>
2022-02-16 14:50:01 +0400

    cors

--- src/server.js
+++ src/server.js
@@ -1,38 +1,26 @@
 const express = require('express')
 require('dotenv').config()
 
-// const cors = require('cors')
+const cors = require('cors')
 const PORT = process.env.PORT || 3332
 const bodyParser = require('body-parser')
 const app = express()
 
-// const corsOptions = {
-// 	origin: '*',
-// 	credentials: true,
-// 	methods: ['GET', 'POST', 'PUT', 'DELETE'],
-// 	allowedHeaders: [ 'Content-Type', 'Authorization', 'Accept', 'Origin', 'X-Requested-With', 'X-HTTP-Method-Override', 'Accept-Language', 'Accept-Encoding', 'Access-Control-Request-Method', 'Access-Control-Request-Headers', 'Access-Control-Allow-Origin', 'Access-Control-Allow-Credentials' ]
-// }
-// app.use(
-// 	cors(corsOptions)
-// )
-app.use(function(req, res, next) {
-	res.setHeader('Access-Control-Allow-Origin', '*')
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
-	res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
-	res.setHeader('Access-Control-Allow-Credentials', true)
-	next()
-})
+const corsOptions = {
+	origin: '*',
+	credentials: true,
+	methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+	allowedHeaders: 'Content-Type, Authorization, Content-Length, X-Requested-With' 
+}
+app.use(
+	cors(corsOptions)
+)
+
 app.use(bodyParser.urlencoded({
 	extended: true
 }))
 app.use(bodyParser.json())
 
-// app.all('*', function(req, res, next) {
-// 	res.header('Access-Control-Allow-Origin', '*')
-// 	res.header('Access-Control-Allow-Headers', 'X-Requested-With')
-// 	res.header('Access-Control-Allow-Headers', 'Content-Type')
-// 	next()
-// })
 
 
 const userRouter = require('./routers/user')
