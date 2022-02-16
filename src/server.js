f5d78fade8ca136fb526e94d3f563898a8a4f762
lashamarkhvaidze <lasha.marxvaidze@gmail.com>
2022-02-16 15:23:27 +0400

    message

--- src/server.js
+++ src/server.js
@@ -6,15 +6,33 @@ const PORT = process.env.PORT || 3332
 const bodyParser = require('body-parser')
 const app = express()
 
+const allowedOrigins = [
+	'capacitor://localhost',
+	'ionic://localhost',
+	'http://localhost',
+	'http://localhost:3000',
+	'http://localhost:3001',
+	'https://greenfrontt-a0qnnq7np-epoqa.vercel.app', 
+	'https://greenfront.vercel.app', 
+	'https://greenfront-i6v43afmn-epoqa.vercel.app',
+	'https://greenfront-qnvofksc5-epoqa.vercel.app'
+
+]
+
+// Reflect the origin if it's in the allowed list or not defined (cURL, Postman, etc.)
 const corsOptions = {
-	origin: '*',
-	credentials: true,
-	methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-	allowedHeaders: 'Content-Type, Authorization, Content-Length, X-Requested-With' 
+	origin: (origin, callback) => {
+		if (allowedOrigins.includes(origin) || !origin) {
+			callback(null, true)
+		} else {
+			callback(new Error('Origin not allowed by CORS'))
+		}
+	},
 }
-app.use(
-	cors(corsOptions)
-)
+
+// Enable preflight requests for all routes
+app.options('*', cors(corsOptions))
+
 
 app.use(bodyParser.urlencoded({
 	extended: true
