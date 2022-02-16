9e578c2b31ab0a3c77ec594f791381ef99b4fed6
lashamarkhvaidze <lasha.marxvaidze@gmail.com>
2022-02-16 14:44:36 +0400

    cors

--- src/server.js
+++ src/server.js
@@ -9,24 +9,30 @@ const app = express()
 const corsOptions = {
 	origin: '*',
 	credentials: true,
-	methods: ['GET', 'POST', 'PUT', 'DELETE'],
-	allowedHeaders: [ 'Content-Type', 'Authorization', 'Accept', 'Origin', 'X-Requested-With', 'X-HTTP-Method-Override', 'Accept-Language', 'Accept-Encoding', 'Access-Control-Request-Method', 'Access-Control-Request-Headers', 'Access-Control-Allow-Origin', 'Access-Control-Allow-Credentials' ]
+	// methods: ['GET', 'POST', 'PUT', 'DELETE'],
+	// allowedHeaders: [ 'Content-Type', 'Authorization', 'Accept', 'Origin', 'X-Requested-With', 'X-HTTP-Method-Override', 'Accept-Language', 'Accept-Encoding', 'Access-Control-Request-Method', 'Access-Control-Request-Headers', 'Access-Control-Allow-Origin', 'Access-Control-Allow-Credentials' ]
 }
 app.use(
 	cors(corsOptions)
 )
-
+app.use(function(req, res, next) {
+	res.setHeader('Access-Control-Allow-Origin', '*')
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+	res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+	res.setHeader('Access-Control-Allow-Credentials', true)
+	next()
+})
 app.use(bodyParser.urlencoded({
 	extended: true
 }))
 app.use(bodyParser.json())
 
-app.all('*', function(req, res, next) {
-	res.header('Access-Control-Allow-Origin', '*')
-	res.header('Access-Control-Allow-Headers', 'X-Requested-With')
-	res.header('Access-Control-Allow-Headers', 'Content-Type')
-	next()
-})
+// app.all('*', function(req, res, next) {
+// 	res.header('Access-Control-Allow-Origin', '*')
+// 	res.header('Access-Control-Allow-Headers', 'X-Requested-With')
+// 	res.header('Access-Control-Allow-Headers', 'Content-Type')
+// 	next()
+// })
 
 
 const userRouter = require('./routers/user')
