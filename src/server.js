19cd640240d6a72b938a53f33a1bdfa67b021636
lashamarkhvaidze <lasha.marxvaidze@gmail.com>
2022-02-16 14:29:25 +0400

    cors er

--- src/server.js
+++ src/server.js
@@ -1,17 +1,33 @@
 const express = require('express')
+require('dotenv').config()
+const domainsFromEnv = process.env.CORS_DOMAINS || ''
+const whitelist = domainsFromEnv.split(',').map(item => item.trim())
+
+console.log(whitelist)
+
+
 const cors = require('cors')
 const PORT = process.env.PORT || 3332
 const bodyParser = require('body-parser')
-
 const app = express()
+
+const corsOptions = {
+	origin: function (origin, callback) {
+		if (!origin || whitelist.indexOf(origin) !== -1) {
+			callback(null, true)
+		} else {
+			callback(new Error('Not allowed by CORS'))
+		}
+	},
+	credentials: true,
+}
 app.use(
-	cors({
-		origin : '*',
-		credentials:true,
-	})
+	cors(corsOptions)
 )
 
-app.use(bodyParser.urlencoded({extended : true}))
+app.use(bodyParser.urlencoded({
+	extended: true
+}))
 app.use(bodyParser.json())
 
 
@@ -24,11 +40,10 @@ require('./db/connection')
 
 
 app.use(express.json())
-app.use(userRouter) 
+app.use(userRouter)
 app.use(diaryRouter)
 app.use(mergeRouter)
- 
+
 app.listen(PORT, () => {
 	console.log(`http://localhost:${PORT}`)
 })
-
